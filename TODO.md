ac7738c1217ba402c5d685f8cd79a9459356e0a6
David Dean <dave.dean@gmail.com>
2021-10-14 10:57:51 +1100

    Implement health endpoint and "foo" endpoint (#6)
    * add test first
    
    * add function with incorrect string to test
    
    * correct output to pass test
    
    * add health handler and implement hello("foo")

--- TODO.md
+++
@@ -1,9 +0,0 @@
-# Possible additions
-
-* [ ] Logging requests/responses
-* [ ] Create multiple endpoint handler(s)
-* [ ] Write to/from a data source (although it's hardly hello world at that point .. )
-* [x] ~~Use a CI tool to test/build (Github Actions?)~~
-* [x] ~~Make Dockerfile~~
-* [x] ~~add unit tests~~
-* [x] ~~make a http hello world~~
--- src/hello-http.go
+++ src/hello-http.go
@@ -7,13 +7,31 @@ import (
 
 func main() {
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, Hello())
-    })
+    http.HandleFunc("/", HandlerDefault)
+    http.HandleFunc("/health", HandlerHealth)
 
     http.ListenAndServe(":8080", nil)
 }
 
-func Hello() string {
-    return "Hello, World"
+func Hello(Helloname string) string {
+    Hellostring := "Hello, " + Helloname
+    return Hellostring
+}
+
+func Health() string {
+    return "OK - 200"
+}
+
+func HandlerDefault(w http.ResponseWriter, r *http.Request) {
+        var Helloname string
+        if r.RequestURI == "/" {
+            Helloname = "World"
+        } else {
+            Helloname = (r.RequestURI)[1:]
+        }
+        fmt.Fprintf(w, Hello(Helloname))
+}
+
+func HandlerHealth(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, Health())
 }
