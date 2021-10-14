5b3fce712d2cc460406f08e5bf00448109c58120
David Dean <dave.dean@gmail.com>
2021-10-14 11:48:12 +1100

    fix linting (#8)

--- src/hello-http.go
+++ src/hello-http.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "log"
     "fmt"
     "net/http"
     "time"
@@ -8,10 +9,13 @@ import (
 
 func main() {
 
+    fmt.Println(niceTime(), "Server start")
+
     http.HandleFunc("/", HandlerDefault)
     http.HandleFunc("/health", HandlerHealth)
 
-    http.ListenAndServe(":8080", nil)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
 
 func Hello(Helloname string) string {
@@ -24,22 +28,22 @@ func Health() string {
 }
 
 func HandlerDefault(w http.ResponseWriter, r *http.Request) {
-        var Helloname string
-        if r.RequestURI == "/" {
-            Helloname = "World"
-        } else {
-            Helloname = (r.RequestURI)[1:]
-        }
-        fmt.Fprintf(w, Hello(Helloname))
-        fmt.Println(niceTime(), "Served ", Helloname)
+    var Helloname string
+    if r.RequestURI == "/" {
+        Helloname = "World"
+    } else {
+        Helloname = (r.RequestURI)[1:]
+    }
+    fmt.Fprintf(w, Hello(Helloname))
+    fmt.Println(niceTime(), "Served ", Helloname)
 }
 
 func HandlerHealth(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, Health())
-        fmt.Println(niceTime(), "Served /health")
+    fmt.Fprintf(w, Health())
+    fmt.Println(niceTime(), "Served /health")
 }
 
 func niceTime () string {
     timeString := time.Now().Format(time.RFC3339)
-        return timeString
+    return timeString
 }
