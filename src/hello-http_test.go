5d0366a526b4224a795c3de2a84f7d7881681e4f
David Dean <dave.dean@gmail.com>
2021-10-18 17:48:53 +1100

    add meme compliant easter egg for Jason (#10)

--- src/hello-http_test.go
+++ src/hello-http_test.go
@@ -12,6 +12,16 @@ func TestHello(t *testing.T) {
     }
 }
 
+func TestHelloJason(t *testing.T) {
+    // Test the 'Hello' function
+    got := Hello("Jason")
+    want := "Say, Jarvis, how much do you know about bitcoin?"
+
+    if got != want {
+        t.Errorf("got %q want %q", got, want)
+    }
+}
+
 func TestHealth(t *testing.T) {
     // Test the 'Health' function
     got := Health()
