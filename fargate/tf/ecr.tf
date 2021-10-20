b2f5d1e624c0e83e88f720cbaf4c545c245a6c00
David Dean <dave.dean@gmail.com>
2021-10-21 09:55:48 +1100

    retab this

--- fargate/tf/ecr.tf
+++ fargate/tf/ecr.tf
@@ -9,33 +9,33 @@ resource "aws_ecr_lifecycle_policy" "go-hello-world-repo-policy" {
 
     policy = <<EOF
 {
-  "rules": [
-    {
-      "rulePriority": 1,
-      "description": "Keep image deployed with tag latest",
-      "selection": {
-        "tagStatus": "tagged",
-        "tagPrefixList": ["latest"],
-        "countType": "imageCountMoreThan",
-        "countNumber": 1
-      },
-      "action": {
-        "type": "expire"
-      }
-    },
-    {
-      "rulePriority": 2,
-      "description": "Keep X older images",
-      "selection": {
-        "tagStatus": "any",
-        "countType": "imageCountMoreThan",
-        "countNumber": 5
-      },
-      "action": {
-        "type": "expire"
-      }
-    }
-  ]
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep image deployed with tag latest",
+            "selection": {
+              "tagStatus": "tagged",
+              "tagPrefixList": ["latest"],
+              "countType": "imageCountMoreThan",
+              "countNumber": 1
+            },
+            "action": {
+                "type": "expire"
+            }
+        },
+        {
+            "rulePriority": 2,
+            "description": "Keep X older images",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "imageCountMoreThan",
+                "countNumber": 5
+            },
+        "action": {
+            "type": "expire"
+        }
+        }
+    ]
 }
 EOF
 }
