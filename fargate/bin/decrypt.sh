817ea1a74cd1802ced0ab03a028070c9ccd3b308
David Dean <dave.dean@gmail.com>
2021-10-21 09:00:26 +1100

    better comments and usage, quote output

--- fargate/bin/decrypt.sh
+++ fargate/bin/decrypt.sh
@@ -1,24 +1,39 @@
 #!/usr/bin/env bash
 
-# decrypts input 
-# outputs decrypted lines
-# could be better, but you get the idea.
-#
+# davedean, 2021
+# decrypt.sh - uses shush to decrypt input and print to output
+# useful for decrypting tfvar files for example.
+
+# Usage ideas: 
+# ./decrypt.sh < input_file 
+# echo SHUSH_secret | ./decrypt.sh 
+
+# Usage example:
+# ../bin/decrypt.sh < backend.tfvars.shush > backend.tfvars
+# ^ will read the .shush file, decrypt, and write a tfvars file
+# ready for use by terraform.
+
 # secrets can be encrypted with shush directly
 # eg: `shush encrypt <keyname> <secret>`
 
+# Output is quoted because thats what I need, ymmv.
 
 while IFS= read -r line; do
     # find one encrypted value in the line
     encrypted=$(echo "$line" | xargs -n 1 | grep SHUSH_ | sed 's/SHUSH_//g' )
     
+    # if we find a SHUSH_ value .. 
     if [ "$encrypted" ] ; then
+        # decrypt it with shush
         decrypted=$(echo $encrypted | shush decrypt)
 
-        replaced=$(echo $line | sed "s|SHUSH_.*$|$decrypted|g" )
+        # rebuild the line, but with SHUSH_ removed and the decrypted value quoted
+        replaced=$(echo $line | sed "s|SHUSH_.*$|\"$decrypted\"|g" )
 
+        # output the rebuilt line
         echo $replaced
     else
+        # if we didn't find a SHUSH_ value, output the original line
         echo $line
     fi
 done
