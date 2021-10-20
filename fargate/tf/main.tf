e01c8a3c0990b7a4b0c1a5a58405abaaf9a33ab7
David Dean <dave.dean@gmail.com>
2021-10-21 09:55:28 +1100

    enable dynamodb locking

--- fargate/tf/main.tf
+++ fargate/tf/main.tf
@@ -11,13 +11,13 @@ terraform {
     }
 
     backend "s3" {
-        // note: bucket = is not specified here, and is provided by backend.tfvars
+        # note: bucket = is not specified here, and is provided by backend.tfvars
         key     = "fg-tf-main"
 
         region  = "ap-southeast-2"
         encrypt = true
 
-        // nb: in a production setting you should use DynamoDB to manage locking
-        // but there's exactly one user of this repo, so not doing it here.
+        # use dynamodb locking
+        dynamodb_table = "fg-tf-main_tf-lock"
     }
 }
