e8b534c3a4e6e9b27a50ec46b60171a29e64cf05
David Dean <dave.dean@gmail.com>
2021-10-21 09:56:48 +1100

    add comments and finish up public subnets and infra

--- fargate/tf/network.tf
+++ fargate/tf/network.tf
@@ -10,6 +10,8 @@ resource "aws_vpc" "fg_tf_vpc" {
     }
 }
 
+### Public Subnets and related infra
+
 # Public Subnets
 resource "aws_subnet" "public_subnet" {
     count                   = length(data.aws_availability_zones.available.names)
@@ -17,54 +19,72 @@ resource "aws_subnet" "public_subnet" {
     cidr_block              = "10.0.${0+count.index}.0/24"
     availability_zone       = data.aws_availability_zones.available.names[count.index]
     map_public_ip_on_launch = true
+}
 
-    tags = {
-        Name = "Public Subnet"
+# Internet GW
+resource "aws_internet_gateway" "internet-gw" {
+    vpc_id = aws_vpc.fg_tf_vpc.id
+}
+
+
+# Routing table for public subnets
+resource "aws_route_table" "route-table_public" {
+    vpc_id = aws_vpc.fg_tf_vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.internet-gw.id
     }
 }
+
+# Assoc route table with public subnets
+resource "aws_route_table_association" "route-assoc_public" {
+    count          = length(data.aws_availability_zones.available.names)
+    subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
+    route_table_id = aws_route_table.route-table_public.id
+}
+
+### End Public Subnets and related infra
+
 
 ### Private Subnets and related infra
 
 # Private Subnets
 resource "aws_subnet" "private_subnet" {
-  count                   = length(data.aws_availability_zones.available.names)
-  vpc_id                  = aws_vpc.fg_tf_vpc.id
-  cidr_block              = "10.0.${40+count.index}.0/24"
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
-  map_public_ip_on_launch = false
-
-    tags = {
-        Name = "Private Subnet"
-    }
+    count                   = length(data.aws_availability_zones.available.names)
+    vpc_id                  = aws_vpc.fg_tf_vpc.id
+    cidr_block              = "10.0.${40+count.index}.0/24"
+    availability_zone       = data.aws_availability_zones.available.names[count.index]
+    map_public_ip_on_launch = false
 }
 
 # Route table for Private Subnets
-resource "aws_route_table" "route_table_private" {
-  vpc_id = aws_vpc.fg_tf_vpc.id
+resource "aws_route_table" "route-table_private" {
+    vpc_id = aws_vpc.fg_tf_vpc.id
 
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.private_nat-gw.id
-  }
+    route {
+        cidr_block     = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.private_nat-gw.id
+    }
 }
 
 # Assoc route table with private subnets
 resource "aws_route_table_association" "private_route" {
-  count          = length(data.aws_availability_zones.available.names)
-  subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
-  route_table_id = aws_route_table.route_table_private.id
+    count          = length(data.aws_availability_zones.available.names)
+    subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
+    route_table_id = aws_route_table.route-table_private.id
 }
 
 # NAT gateway requires an EIP
 resource "aws_eip" "private_nat-gw_eip" {
-  vpc = true
+    vpc = true
 }
 
 # NAT Gateway
 resource "aws_nat_gateway" "private_nat-gw" {
-  allocation_id = aws_eip.private_nat-gw_eip.id
-  subnet_id     = element(aws_subnet.private_subnet.*.id, 1)
-  # depends_on    = ["aws_internet_gateway.myInternetGateway"]
+    allocation_id = aws_eip.private_nat-gw_eip.id
+    subnet_id     = element(aws_subnet.private_subnet.*.id, 1)
+    depends_on    = [ aws_internet_gateway.internet-gw ]
 }
 
 ### End Private Subnets and related infra
