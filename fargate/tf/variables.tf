b56a13bdaea77e003db8d44adb3f59a2009ef869
David Dean <dave.dean@gmail.com>
2021-10-22 15:57:45 +1100

    switch to splitting the cidr using logic not strings (#27)

--- fargate/tf/variables.tf
+++ fargate/tf/variables.tf
@@ -2,10 +2,6 @@ variable "vpc_cidr-block" {
     default = "10.0.0.0/16"
 }
 
-variable "vpc_cidr-block_fragment" {
-    default = "10.0."
-}
-
 variable "environment" {
 }
 
@@ -13,4 +9,12 @@ variable "environment_short-code" {
 }
 
 variable "service_name" {
+}
+
+variable "vpc_subnet-splits" {
+    default = "4"
+}
+
+variable "subnet-splits" {
+    default = "4"
 }
