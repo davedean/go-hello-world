9b8f551b841e04a8ffb7a35388c4352c2b82e25d
David Dean <dave.dean@gmail.com>
2021-10-26 16:09:17 +1100

    renaming to be more consistent

--- fargate/tf/alb.tf
+++ fargate/tf/alb.tf
@@ -1,5 +1,5 @@
 resource "aws_lb" "alb_private" {
-    name               = "alb"
+    name               = "alb-private"
     subnets            = aws_subnet.private_subnet.*.id
     load_balancer_type = "application"
     security_groups    = [aws_security_group.sg_alb_private.id]
@@ -11,14 +11,14 @@ resource "aws_lb_listener" "https-fwd_private" {
     protocol          = "HTTP"
 
     default_action {
-        target_group_arn = aws_lb_target_group.lb-tg_private.arn
+        target_group_arn = aws_lb_target_group.alb-tg_private.arn
         type = "forward"
      }
 
 }
 
-resource "aws_lb_target_group" "lb-tg_private" {
-    name        = "alb-tg"
+resource "aws_lb_target_group" "alb-tg_private" {
+    name        = "alb-private-tg"
     port        = 8080
     protocol    = "HTTP"
     vpc_id      = aws_vpc.fg_tf_vpc.id
